76f35e05ba2c1e11e6b53763e238ef5065640037
HeroEpic <warriorknightpro@gmail.com>
2025-08-01 13:46:20 -0300

    Update build.sh

--- packages/gst-libav/build.sh
+++ packages/gst-libav/build.sh
@@ -1,5 +1,5 @@
 PKG_VER=1.24.11
 SRC_URL=https://gstreamer.freedesktop.org/src/gst-libav/gst-libav-$PKG_VER.tar.xz
-MESON_ARGS="-Dtests=disabled"
+MESON_ARGS="-Dtests=disabled --disable-bzlib --disable-lzma"
 CFLAGS="-I$PREFIX/include"
 LDFLAGS="-L$PREFIX/lib"
