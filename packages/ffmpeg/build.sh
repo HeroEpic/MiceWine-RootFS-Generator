2e7e7bd43dff439dd900c049125fe34db5df0b4e
HeroEpic <warriorknightpro@gmail.com>
2025-08-01 19:37:00 -0300

    Update build.sh

--- packages/ffmpeg/build.sh
+++ packages/ffmpeg/build.sh
@@ -1,3 +1,3 @@
 PKG_VER=n4.4.6
 SRC_URL=https://github.com/FFmpeg/FFmpeg/archive/refs/tags/$PKG_VER.tar.gz
-CONFIGURE_ARGS="--disable-doc"
+CONFIGURE_ARGS="--disable-doc --disable-bzlib --disable-lzma"
