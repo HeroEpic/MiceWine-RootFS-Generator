3d9b63b4d097cf09b64e2e32e65b9b3cf6daf439
HeroEpic <warriorknightpro@gmail.com>
2025-08-01 23:44:08 -0300

    Update build.sh

--- packages/ffmpeg/build.sh
+++ packages/ffmpeg/build.sh
@@ -2,4 +2,4 @@ BLACKLIST_ARCH=x86_64
 
 PKG_VER=n4.4.6
 SRC_URL=https://github.com/FFmpeg/FFmpeg/archive/refs/tags/$PKG_VER.tar.gz
-CONFIGURE_ARGS="--disable-doc --disable-bzlib --disable-lzma --enable-cross-compile --sysroot=$SYSROOT --arch=$ARCH --disable-static --enable-shared"
+CONFIGURE_ARGS="--disable-doc --disable-bzlib --disable-lzma --enable-cross-compile --sysroot=$SYSROOT --arch=$ARCH --target-os=android --cpu=armv8-a --disable-static --enable-shared"
