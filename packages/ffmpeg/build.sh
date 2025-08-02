064af5af403bab647e8008d482bd41459efcf74b
HeroEpic <warriorknightpro@gmail.com>
2025-08-02 17:06:57 -0300

    Update build.sh

--- packages/ffmpeg/build.sh
+++ packages/ffmpeg/build.sh
@@ -4,4 +4,4 @@ PKG_VER=n4.4.6
 SRC_URL=https://github.com/FFmpeg/FFmpeg/archive/refs/tags/$PKG_VER.tar.gz
 CONFIGURE_ARGS="--disable-doc --disable-bzlib --disable-lzma --enable-cross-compile --target-os=android --cc=$CC --arch=$ARCH --sysroot=$SYSROOT --enable-shared"
 CFLAGS="-I$PREFIX/include"
-LDFLAGS="-L$PREFIX/lib -landroid_shmem"
+LDFLAGS="-L$PREFIX/lib -l:libandroid_shmem.so"
